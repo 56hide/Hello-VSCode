d7fbb2fa68acfb72cf0ee3055c20bcde6960194c
56hide <1748110455@qq.com>
2018-03-18 22:41:17 +0800

    03-18test

--- demo.py
+++ demo.py
@@ -1,1 +1,14 @@
-print("hello VSCode!")
+# a = 'abc'
+# b = a
+# a = 'ABC'
+# print(b)
+# n = 123
+# f = 456.789
+# s1 = 'Hello, world'
+# s2 = 'Hello, \'Adam\''
+# s3 = r'Hello, "Bart"'
+# s4 = r'''Hello,
+# Lisa!'''
+# print(s3)
+a = 'ABC'.encode('ascii')
+print(a)
