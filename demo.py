31a5a177ce8b717d0c471a7cc631f1b0880264c3
56hide <1748110455@qq.com>
2018-03-19 10:35:29 +0800

    10.35hourtest

--- demo.py
+++ demo.py
@@ -12,3 +12,35 @@
 # print(s3)
 # a = 'ABC'.encode('ascii')
 # print(a)
+# for i in range(0,5):
+#     print(i)
+# from random import randint
+# i = randint(0,2)
+# a = input()
+# bingo = True
+# while(a==i):
+#     print("Bingo")
+# num = 10
+# print("guess what i think?")
+# answer = input()
+# if answer<num:
+#     print("too small")
+# if answer>num:
+#     print("too big")
+# if answer==num:
+#     print("bingo")
+num = 10
+print("Guess  what i think?")
+bingo = False
+
+while bingo==False:
+    answer = input()
+    if answer < num:
+        print("too small!")
+    if answer > num:
+        print("too big")
+    if answer == num:
+        print("BINGO!")
+        bingo = True
+    
+
